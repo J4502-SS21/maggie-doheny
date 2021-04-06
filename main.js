49e9acea2e232a37ebfd6db1cca22c82dcbc1950
maggiedoheny <mmdrnb@umsystem.edu>
2021-04-05 23:25:03 -0500

    Added Javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var menuNav = document.getElementById("menu_nav");
+
+var menuTrigger = document.getElementById("menu_trigger");
+
+function menuToggle() {
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+  } else {
+    menuNav.style.display = "flex";
+  }
+  console.log("The menu has been toggled!");
+}
+
+menuTrigger.onclick = menuToggle;
